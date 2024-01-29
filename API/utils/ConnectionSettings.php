3694dd15ddae8f6d8aeec3f881ef12d1a3a3a324
SP Singh <19812199+spcheema@users.noreply.github.com>
2024-01-30 07:06:41 +1100

    Clean up connection settings file

--- API/utils/ConnectionSettings.php
+++ API/utils/ConnectionSettings.php
@@ -8,22 +8,7 @@ $filesRoot = '/var/www/html/Files';
 $websocketPort = '8010';
 $externalWebsocketPort = '443';
 $externalWebsocketHost = 'wss.ttapps.org';
-
-//$dns = "mysql:host={$GLOBALS['serverName']};dbname={$GLOBALS['databaseName']}";
-
 define('ROOT_PATH', dirname(__DIR__) . '/../');
-
-
-$dbPath = ROOT_PATH . $databaseName . '.db';
-
-// TODO duplicate variable, restructure into test config
-$filesRoot = ROOT_PATH . 'Files';
-
-$dns = "sqlite:$dbPath";
-
-define('DB_FILE_PATH', $dbPath);
-
-define('DB_DNS', $dns);
 
 #  This is for dev 10.10.10.248 server
 #$databaseUser = 'dharding';
@@ -32,3 +17,6 @@ define('DB_DNS', $dns);
 #$websocketPort = '8082';
 #$externalWebsocketPort = '8082';
 #$externalWebsocketHost = '10.10.10.248';
+
+$dns = "mysql:host={$serverName};dbname={$databaseName}";
+define('DB_DNS', $dns);
